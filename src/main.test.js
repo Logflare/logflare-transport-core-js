fd091d30e2060fccdde63a629ac063d1c3777717
Kang Ming <kang.ming1996@gmail.com>
2023-06-07 17:02:19 +0800

    add test for onError callback

--- src/main.test.js
+++ src/main.test.js
@@ -7,10 +7,18 @@ const apiResponseSuccess = {message: "Logged!"}
 
 describe("LogflareHttpClient", () => {
     let httpClient
+    let httpClientOnError
     let nativeFetch
+    let consoleErrorData = []
+    const storeLog = (inputs) => consoleErrorData.push(inputs)
+    const onErrorCallback = jest.fn((payload, err) => {
+        console.error(payload)
+        console.error(err)
+    })
 
     beforeAll(() => {
         nativeFetch = global.fetch
+        console["error"] = jest.fn(storeLog)
     })
 
     beforeEach(() => {
@@ -19,10 +27,17 @@ describe("LogflareHttpClient", () => {
             sourceToken: testSourceToken,
             apiBaseUrl: "http://non-existing.domain",
         })
+        httpClientOnError = new LogflareHttpClient({
+            apiKey: testApiKey,
+            sourceToken: testSourceToken,
+            apiBaseUrl: "http://non-existing.domain",
+            onError: onErrorCallback,
+        })
     })
 
     afterEach(() => {
         global.fetch.mockClear()
+        consoleErrorData = []
     })
 
     afterAll(() => {
@@ -55,12 +70,9 @@ describe("LogflareHttpClient", () => {
         )
     })
 
-    let consoleLogData = ""
-    const storeLog = (inputs) => (consoleLogData += inputs)
     it("prints to console on error", async () => {
         const errorResponse = {message: "Schema validation error"}
 
-        console["error"] = jest.fn(storeLog)
         global.fetch = jest.fn(() =>
             Promise.resolve({
                 json: () => Promise.resolve(errorResponse),
@@ -72,10 +84,38 @@ describe("LogflareHttpClient", () => {
         const le = {message: "info log msg", metadata: {p1: "v1"}}
 
         await httpClient.addLogEvent(le)
-        expect(consoleLogData).toBe(
+        expect(consoleErrorData[0]).toBe(
             `Logflare API request failed with 406 status: ${JSON.stringify(
                 errorResponse
             )}`
         )
+    })
+
+    it("invoke onError callback on error", async () => {
+        const errorResponse = {message: "Schema validation error"}
+
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                json: () => Promise.resolve(errorResponse),
+                ok: false,
+                status: 406,
+            })
+        )
+
+        const le = {message: "info log msg", metadata: {p1: "v1"}}
+
+        await httpClientOnError.addLogEvent(le)
+        const [message, payload, err] = consoleErrorData
+        expect(message).toBe(
+            `Logflare API request failed with 406 status: ${JSON.stringify(
+                errorResponse
+            )}`
+        )
+        expect(onErrorCallback).toHaveBeenCalledTimes(1)
+        expect(payload).toStrictEqual({
+            batch: [le],
+        })
+        expect(err.message).toMatch(/Network response was not ok for/)
+        expect(err.data).toBe(errorResponse)
     })
 })
